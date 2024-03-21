8f61d84abf8df9523a90ace2b78bace1467920cf
SimonBerghmans <simon.berghmans@student.kdg.be>
2024-03-21 20:14:44 +0100

    large files to github

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -26,12 +26,14 @@ public class GameManager : Singleton
     private bool roundTimerRunning;
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         SetGameManagerValues();
         SetPlayerLocation();
         UpdateRoundInfoText();
     }
+
+   
  
     // Update is called once per frame
     void Update()
